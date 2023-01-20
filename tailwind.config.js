3efbb15992d07995f92ddf9f907c5fb8944f3831
Andrew Ponomarev <spargo32@gmail.com>
2023-01-20 15:13:31 +0300

    Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+    ],
+    theme: {
+
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+}
